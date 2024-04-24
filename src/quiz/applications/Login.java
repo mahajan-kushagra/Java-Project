44e889646408024feb80a64a41aaa1941f2bc4b1
Kushagra <connect.kushagramahajan@gmail.com>
2024-04-24 16:08:53 +0530

    Added final files

--- src/quiz/applications/Login.java
+++ src/quiz/applications/Login.java
@@ -1,28 +1,50 @@
 package quiz.applications;
 
-import javax.swing.*;
-import java.awt.event.*;
-import java.awt.*;
+import javax.swing.*; // getContentPane, JFrame, JLabel, JTextField, JButton, JPasswordField, JOptionPane, ImageIcon, Color, Font,
+import java.awt.event.*; 
+import java.awt.*;  // ActionListener
+// inheritence to extend JFrame class
+// JFrame class is in swing package so we have to import swing
+// and this swing class comes under javax package
+// ActionListener comes under java awt event package
+// graphical user interface
+
+// encapsulation - jbuttons, jtextfield, jlabel etc encapsulate gui components
 
 public class Login extends JFrame implements ActionListener {
     JButton proceed, back, register;
     JTextField tf_username, tf_password;
+    // here we are defining it globally because we want to 
+    // add a click event on it 
+    // login class constructor
+    // actionlistener interface of gui components 
 
     Login() {
-        setTitle("Login Page");
-        getContentPane().setBackground(Color.WHITE);
-        setLayout(null);
 
+        setTitle("Login Page"); // it will show on the top left corner of the frame
+        // color of frame
+        getContentPane().setBackground(Color.WHITE); //In Java Swing, 
+        // the layer that is used to hold objects is called the content pane. 
+        setLayout(null);
+         // color class is in java awt package
+        // to make a custome layout first we set the default layout as none
         JLabel image = new JLabel(new ImageIcon(ClassLoader.getSystemResource("icons/login.jpeg")));
         image.setBounds(0, 0, 600, 500);
+         // image 
+        // classLoader is an abstract class which belongs to java.lang package
+
+        
         add(image);
 
+// heading 
         JLabel heading = new JLabel("Simple minds");
         heading.setBounds(750, 60, 300, 45);
         heading.setFont(new Font("Viner Hand ITC", Font.BOLD, 40));
         heading.setForeground(new Color(30, 144, 254));
         add(heading);
+    // JLabel inherits JComponent class which is in javax package
 
+    // username text field
         JLabel name = new JLabel("Username");
         name.setBounds(685, 150, 200, 25);
         name.setFont(new Font("Arial", Font.BOLD, 18));
@@ -33,7 +55,8 @@ public class Login extends JFrame implements ActionListener {
         tf_username.setBounds(800, 150, 300, 25);
         tf_username.setFont(new Font("times New Roman", Font.BOLD, 18));
         add(tf_username);
-
+        
+// password text field
         JLabel password = new JLabel("Password");
         password.setBounds(685, 200, 200, 25);
         password.setFont(new Font("Arial", Font.BOLD, 18));
@@ -44,7 +67,7 @@ public class Login extends JFrame implements ActionListener {
         tf_password.setBounds(800, 200, 300, 25);
         tf_password.setFont(new Font("times New Roman", Font.BOLD, 20));
         add(tf_password);
-
+// proceed button
         proceed = new JButton("Proceed");
         proceed.setBounds(775, 270, 120, 25);
         proceed.setBackground(new Color(30, 144, 254));
@@ -52,65 +75,113 @@ public class Login extends JFrame implements ActionListener {
         proceed.addActionListener(this);
         add(proceed);
 
+ //back button
+
         back = new JButton("Back");
         back.setBounds(955, 270, 120, 25);
         back.setBackground(new Color(30, 144, 254));
         back.setForeground(Color.WHITE);
-        back.addActionListener(this);
+// click event on back button      
+        back.addActionListener(this); // this can be used as an argument in the method
         add(back);
-
+// new user text
         JLabel newUser = new JLabel("New user? ");
         newUser.setBounds(685, 330, 200, 25);
         newUser.setFont(new Font("Arial", Font.BOLD, 14));
         newUser.setForeground(new Color(30, 144, 254));
         add(newUser);
-
+//  register button
         register = new JButton("Register");
         register.setBounds(770, 330, 120, 25);
         register.setBackground(new Color(30, 144, 254));
         register.setForeground(Color.WHITE);
-        register.addActionListener(this);
+        // click event on register button
+        register.addActionListener(this); // here we have used this keyword as a parameter in the method
         add(register);
-
+// frame 
+//  // the default visibility of frame is false
+// the frame will show at the top left corner of the screen
+ // to set dimension of frame we have to use setSize() method
         setSize(1200, 500);
         setLocation(200, 200);
         setVisible(true);
     }
-
+// if tap on back button we want to show the login page again 
     void showPane() {
-        tf_password.setText("");
-        setVisible(true);
+        tf_password.setText(""); // when back then password will not be shown
+        setVisible(true); // since we need to show the login form again on the screen
     }
-
+ // this is the method of action listener interface which is implemented by the class
+    // if we implement any interface in our class then all the unimplemented methods 
+    // has to be overriden
+@Override
     public void actionPerformed(ActionEvent ae) {
+         // first we have to differentiate which button has been clicked
+    // because same function has been called on both the buttons
         if (ae.getSource() == back) {
-            setVisible(false);
-        } else if (ae.getSource() == proceed) {
-            if (tf_username.getText().equals("")) {
-                JOptionPane.showMessageDialog(this, "Please enter username first!", "Error",JOptionPane.ERROR_MESSAGE);
-            } else if (tf_password.getText().equals("")) {
-                JOptionPane.showMessageDialog(this, "Please enter password!", "Error", JOptionPane.ERROR_MESSAGE);
-            } else {
-                try {
-                    if (Credentials.validate(tf_username.getText(), tf_password.getText())) {
-                        setVisible(false);
-                        new Rules(tf_username.getText(), this);
-                    } else {
-                        JOptionPane.showMessageDialog(this, "Incorrect password!", "Error", JOptionPane.ERROR_MESSAGE);
+            setVisible(false); // if back button is used
+        } else if (ae.getSource() == proceed) 
+        //The getSource method is used in the actionPerformed method to determine which button was clicked.
+            {  // if proceed button is used
+                if (tf_username.getText().equals("")) // if the username is empty
+                {
+                    JOptionPane.showMessageDialog(this, "Please enter username first!", "Error",JOptionPane.ERROR_MESSAGE);
+                } 
+                else if (tf_password.getText().equals("")) // if password is empty
+                { //joptionpane class is used to provide dialog boxes such as msg dialog box
+                    JOptionPane.showMessageDialog(this, "Please enter password!", "Error", JOptionPane.ERROR_MESSAGE);
+                } 
+                // here this refers to the current object or instance of the class 
+                // here it refers to the current jframe instance, which is the parent component for the dialog
+                else // if both text fiels are filled
+                {
+                    try { // we hav3 eused exception handling here
+                        // The validate method can be called each time you are about to 
+                        // throw the object to the server for some processing, and if it returns true , you can go ahead.
+
+                        if (Credentials.validate(tf_username.getText(), tf_password.getText()))  
+                         {
+                            setVisible(false);
+                            new Rules(tf_username.getText(), this);
+                        } 
+                        else 
+                        {
+                            JOptionPane.showMessageDialog(this, "Incorrect password!", "Error", JOptionPane.ERROR_MESSAGE);
+                        }
+                } 
+                    catch(FileDeletedException fdEx) 
+                    {
+                        JOptionPane.showMessageDialog(this, fdEx, "Error", JOptionPane.ERROR_MESSAGE);
+                    } 
+                    catch (UserDoesNotExistsException udeEx)
+                    {
+                        JOptionPane.showMessageDialog(this, udeEx, "Error", JOptionPane.ERROR_MESSAGE);
                     }
-                } catch(FileDeletedException fdEx) {
-                    JOptionPane.showMessageDialog(this, fdEx, "Error", JOptionPane.ERROR_MESSAGE);
-                } catch (UserDoesNotExistsException udeEx) {
-                    JOptionPane.showMessageDialog(this, udeEx, "Error", JOptionPane.ERROR_MESSAGE);
-                }
             }
-        } else if (ae.getSource() == register) {
+        } 
+            else if (ae.getSource() == register) 
+            {
             setVisible(false);
             new Register(this);
-        }
+            }
     }
 
     public static void main(String[] args) {
         new Login();
+
+        // a new object named login, annonymous object
+        // we want that as soon as i run the class, a frame will show up
+        // class ko run karte se hi main method call hota hai
+        //class ka object banate se hi constructor call hota hai
+        // only if we write the code part in constructor we will gonna see the code 
+        // just after calling the class
+
+        // Anonymous Object - the memory assigned for anonymous object is in only heap area
+        // and not in stack
+        // if we want to create a frame and show it on the screen we have to create an object of frame class
+        // anonymous objects can be called once only
+        // if we want to call the object again we have to create a new object
+        // it is not a referenced object
     }
+
 }
--- src/quiz/applications/Quiz.java
+++ src/quiz/applications/Quiz.java
@@ -1,27 +1,27 @@
 package quiz.applications;
 
 import java.awt.*;
-import javax.swing.*;
-import java.awt.event.*;
+import javax.swing.*;//jframe class is in this package
+import java.awt.event.*;//color class is in this package,ActionListener interface that comes in awt.event package
 
-public class Quiz extends JFrame implements ActionListener {
+public class Quiz extends JFrame implements ActionListener {//actionListener handles the event like we click on next buttons or submit
     // actionListner we get form event package
-    String questions[][] = new String[10][5];
-    String answers[][] = new String[10][2];
-    String useranswers[][] = new String[10][1];
+    String questions[][] = new String[10][5];//we have created a 2d array for storing questions of string type each question in a row has 5 options in the form of column
+    String answers[][] = new String[10][2];//storing answers in the form of 2d array with 2 column 1 for correct options we can make 10*1 array also
+    String useranswers[][] = new String[10][1];//user answers are stored
     JRadioButton opt1, opt2, opt3, opt4;
-    JLabel qno, question;
+    JLabel qno, question;//globally declared
     ButtonGroup groupOptions;
     JButton next, submit;
-    public static int timer = 15;
-    public static int ans_given = 0;
+    public static int timer = 15;//static variable -it is a class variable it is created only once and shared by all objects of the class.it is use for memory management
+    public static int ans_given = 0;//flag value initially to 0 and check it the user has answered any option or not
     public static int count = 0;
     public static int score = 0;
-    String name;
+    String name;//g;lobally declared
 
     Quiz(String name) {
-        this.name = name;
-        setBounds(50, 0, 1440, 850);
+        this.name = name;//stored globally for the other class to use
+        setBounds(50, 0, 1440, 850);  
 
         getContentPane().setBackground(Color.WHITE);
         setLayout(null);
@@ -32,9 +32,9 @@ public class Quiz extends JFrame implements ActionListener {
         image.setBounds(0, 0, 1440, 392);
         add(image);
 
-        qno = new JLabel();
+        qno = new JLabel(); //values are inserted dynamically
         qno.setBounds(100, 450, 50, 30);
-        qno.setFont(new Font("Tahoma", Font.PLAIN, 24));
+        qno.setFont(new Font("Tahoma", Font.PLAIN, 24));//created a font class object
         add(qno);
 
         question = new JLabel("");
@@ -42,11 +42,11 @@ public class Quiz extends JFrame implements ActionListener {
         question.setFont(new Font("Tahoma", Font.PLAIN, 24));
         add(question);
 
-        questions[0][0] = "Which one of the following is not a Java feature?";
-        questions[0][1] = "Object-oriented";
-        questions[0][2] = "Use of pointers";
-        questions[0][3] = "Portable";
-        questions[0][4] = "Dynamic and Extensible";
+        questions[0][0] = "Which is used to find and fix bugs in the Java programs.?";
+        questions[0][1] = "JVM";
+        questions[0][2] = "JDB";
+        questions[0][3] = "JDK";
+        questions[0][4] = "JRE";
 
         questions[1][0] = "What is the return type of the hashCode() method in the Object class?";
         questions[1][1] = "int";
@@ -54,11 +54,11 @@ public class Quiz extends JFrame implements ActionListener {
         questions[1][3] = "long";
         questions[1][4] = "void";
 
-        questions[2][0] = "What is not the use of “this” keyword in Java?";
-        questions[2][1] = "Referring to the instance variable when a local variable has the same name";
-        questions[2][2] = "Passing itself to the method of the same class";
-        questions[2][3] = "Passing itself to another method";
-        questions[2][4] = "Calling another constructor in constructor chaining";
+        questions[2][0] = "Which package contains the Random class?";
+        questions[2][1] = "java.util package";
+        questions[2][2] = "java.lang package";
+        questions[2][3] = "java.awt package";
+        questions[2][4] = "java.io package";
 
         questions[3][0] = "An interface with no fields or methods is known as?";
         questions[3][1] = "Runnable Interface";
@@ -78,11 +78,11 @@ public class Quiz extends JFrame implements ActionListener {
         questions[5][3] = "Readable interface";
         questions[5][4] = "Result interface";
 
-        questions[6][0] = "Which of the following is a superclass of every class in Java?";
-        questions[6][1] = "ArrayList";
-        questions[6][2] = "Abstract class";
-        questions[6][3] = "Object class";
-        questions[6][4] = "String";
+        questions[6][0] = "Which keyword is used for accessing the features of a package?";
+        questions[6][1] = "import";
+        questions[6][2] = "package";
+        questions[6][3] = "extends";
+        questions[6][4] = "export";
 
         questions[7][0] = "In java, jar stands for?";
         questions[7][1] = "Java Archive Runner";
@@ -102,18 +102,18 @@ public class Quiz extends JFrame implements ActionListener {
         questions[9][3] = "Use of exception handling";
         questions[9][4] = "Dynamic binding between objects";
 
-        answers[0][1] = "Use of pointers";
+        answers[0][1] = "JDB";
         answers[1][1] = "int";
-        answers[2][1] = "Passing itself to the method of the same class";
+        answers[2][1] = "java.util package";
         answers[3][1] = "Marker Interface";
         answers[4][1] = "Heap memory";
         answers[5][1] = "Remote interface";
-        answers[6][1] = "Object class";
+        answers[6][1] = "import";
         answers[7][1] = "Java Archive";
         answers[8][1] = "java.lang.StringBuilder";
         answers[9][1] = "Bytecode is executed by JVM";
 
-        opt1 = new JRadioButton();
+        opt1 = new JRadioButton();//for the selection of option
         opt1.setBounds(170, 520, 700, 30);
         opt1.setBackground(Color.WHITE);
         opt1.setFont(new Font("Dialog", Font.PLAIN, 20));
@@ -147,9 +147,9 @@ public class Quiz extends JFrame implements ActionListener {
         next = new JButton("Next");
         next.setBounds(1100, 550, 200, 40);
         next.setFont(new Font("Tahoma", Font.PLAIN, 22));
-        next.setBackground(new Color(30, 144, 255));
+        next.setBackground(new Color(30, 144, 255));//if we want to set values inn rgv formet and color class object we have to create
         next.setForeground(Color.WHITE);
-        next.addActionListener(this);
+       
         add(next);
 
         submit = new JButton("Submit");
@@ -165,7 +165,7 @@ public class Quiz extends JFrame implements ActionListener {
     }
 
     public void actionPerformed(ActionEvent ae) {
-        if (ae.getSource() == next) {
+        if (ae.getSource() == next) {//if next button is called then repaint 
             repaint();
             opt1.setEnabled(true);
             opt2.setEnabled(true);
@@ -190,7 +190,8 @@ public class Quiz extends JFrame implements ActionListener {
             if (groupOptions.getSelection() == null) {
                 useranswers[count][0] = "";
             } else {
-                useranswers[count][0] = groupOptions.getSelection().getActionCommand();// getactioncommand peforms the action
+                useranswers[count][0] = groupOptions.getSelection().getActionCommand();// getactioncommand peforms the
+                                                                                       // action
             }
             for (int i = 0; i < useranswers.length; i++) {
                 if (useranswers[i][0].equals(answers[i][1]))// string comparison the actual answer ans the answer that user has marked.
@@ -203,25 +204,29 @@ public class Quiz extends JFrame implements ActionListener {
         }
     }
 
-    public void paint(Graphics g) {
-        super.paint(g);
-        String time = "Time left - " + timer + " seconds ";
+    public void paint(Graphics g) {//paint method is used which take the object of the graphics class as an argument
+        super.paint(g);// we are calling the paint method in the super class  and passing graphics class onject that is g
+        String time = "Time left - " + timer + " seconds ";//graphics
         g.setColor(Color.RED);
         g.setFont(new Font("Tahoma", Font.BOLD, 25));
-        if (timer > 0) {
+        if (timer > 0) {//paint method calls by itself we do not have tto call it separatly
             g.drawString(time, 1100, 500);
         } else {
             g.drawString("Times up!!", 1100, 500);
         }
         timer--;
         try {
-            Thread.sleep(1000); // 1 thousand=1000millisecond
+            Thread.sleep(1000); // 1 thousand=1000millisecond we have to stop the timer for 1 sec for this we have used thread class sleep method,thread.sleep( method to pause the execution of the curreent thread)
             repaint();
+            //thread.sleep( can throw interruptedException,if the thread is interrupted while it is sleeping)
+            //after the thread is paused for 1 sec the repaint() method is caeed thid method is likely part of a gui framework such as swung of javaFX and is used to request the the gui component should be redrawn
         } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        if (ans_given == 1) {
+            e.printStackTrace();//if the exception occurs within the try blCK THIS LINE PRINTS THE information about the exception to the console.it is using the printStacktrace() method of the Exception  object e to print the stack trace which included information about where the
+            // exception occured and what caused it
+        }//so the try-catch block is sued here to ensure that if an exception occures during the call to thread.sleep() the program does not crash abruptly and instead it can handle the exception appropriately such as printing a stack trace
+//The throw keyword is used to explicitly 
+//throw an exception from within a method or block of code. However, in the provided code snippet, there is no explicit need to throw an exception because the Thread.sleep(1000) method itself already throws an exception (InterruptedException) if the sleeping thread is interrupted.
+        if (ans_given == 1) {//if and is given then set flag to 0
             ans_given = 0;
             timer = 15;
         } else if (timer < 0) {
@@ -237,10 +242,10 @@ public class Quiz extends JFrame implements ActionListener {
             if (count == 9) // auto submit if user dont click the sumbit button
             {
                 if (groupOptions.getSelection() == null) {
-                    useranswers[count][0] = "";
+                    useranswers[count][0] = "";//if no answer is given by user then store empty value
                 } else {
                     useranswers[count][0] = groupOptions.getSelection().getActionCommand();// getactioncommand peforms
-                                                                                           // the action
+                                                                                           // the action whatever option the user has choosen will stored in useranswer array
                 }
                 for (int i = 0; i < useranswers.length; i++) {
                     if (useranswers[i][0].equals(answers[i][1]))// string comparison the actul answer ans the anaswer
@@ -250,8 +255,9 @@ public class Quiz extends JFrame implements ActionListener {
                     else
                         score += 0;
                 }
-                setVisible(false);
-                new Score(name, score);
+                setVisible(false);//current frame will get invisible and the new score class frame will get open 
+
+                new Score(name, score);//calling of score class
             } else {
                 if (groupOptions.getSelection() == null) {
                     useranswers[count][0] = "";
@@ -274,17 +280,17 @@ public class Quiz extends JFrame implements ActionListener {
     }
 
     public void start(int count) {
-        qno.setText((count + 1) + ". ");
-        question.setText(questions[count][0]);
+        qno.setText((count + 1) + ". ");//this line of code sets the text of the qno object to a string that represent a numbered list .
+        question.setText(questions[count][0]);//pick the value from first row and first column
         opt1.setText(questions[count][1]);
-        opt1.setActionCommand(questions[count][1]);
+        opt1.setActionCommand(questions[count][1]);// set the option with question
         opt2.setText(questions[count][2]);
         opt2.setActionCommand(questions[count][2]);
         opt3.setText(questions[count][3]);
         opt3.setActionCommand(questions[count][3]);
         opt4.setText(questions[count][4]);
         opt4.setActionCommand(questions[count][4]);
-        groupOptions.clearSelection();
+        groupOptions.clearSelection();//suppose we have selected one option then after switching to the next option that option will be deselected 
     }
 
     public static void main(String[] args) {
